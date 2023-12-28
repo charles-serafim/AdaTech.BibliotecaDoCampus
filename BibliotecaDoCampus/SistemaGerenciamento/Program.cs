c5b8e4b930ab845aa1a3df87784e8a2ed5585d0b
Charles Serafim Morais <charles.serafim.morais@gmail.com>
2023-12-27 21:23:36 -0300

    feat[SistemaGerenciamento]: adiciona solucao e projeto SistemaGerenciamento

---
+++ BibliotecaDoCampus/SistemaGerenciamento/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaGerenciamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema de Biblioteca do Campus");
+        }
+    }
+}
