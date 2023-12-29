47eb6a3ba1f9e303082af63156bf420577a24fac
Charles Serafim Morais <charles.serafim.morais@gmail.com>
2023-12-29 01:03:16 -0300

    feat: adicionando dummies e adicao de livros

---
+++ BibliotecaDoCampus/SistemaGerenciamento/Livro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGerenciamento;
+
+public class Livro
+{
+    private static int s_contadorLivro = 0;
+    public int IdLivro { get; set; }
+    public string _titulo;
+    public string _autores;
+    public int _edicao;
+    public Acervo _acervo;
+    public EstadoLivro _estadoLivro;
+
+    public Livro(string titulo, string autores, int edicao, Acervo acervo, EstadoLivro estadoLivro)
+    {
+        IdLivro = ++s_contadorLivro;
+        _titulo = titulo;
+        _autores = autores;
+        _edicao = edicao;
+        _acervo = acervo;
+        _estadoLivro = estadoLivro;
+    }
+
+    public void MostrarDados()
+    {
+        Console.WriteLine($"Id do livro: {IdLivro}\n"
+                        + $"Título: {_titulo}\n"
+                        + $"Autor: {_autores}\n"
+                        + $"Edição: {_edicao}a edição\n"
+                        + $"Condição: {_estadoLivro}\n"
+                        + $"Acervo: {_acervo}\n");
+    }
+}
--- BibliotecaDoCampus/SistemaGerenciamento/Program.cs
+++ BibliotecaDoCampus/SistemaGerenciamento/Program.cs
@@ -9,6 +9,15 @@ internal class Program
             new Usuario("Monalisa Brito", "m.b@mail.com", NivelAcesso.Diretor)
         };
 
+    static List<Livro> listaDeLivros = new List<Livro>()
+        {
+            new Livro( "Compiladores: Princípios, Técnicas e Ferramentas", "Alfred V. Aho, Monica S. Lam, Ravi Sethi, Jeffrey D. Ullman", 1, Acervo.AcervoRestrito, EstadoLivro.Disponível ),
+            new Livro( "O Hobbit", "J. R. R. Tolkien", 3, Acervo.AcervoPublico, EstadoLivro.Reservado ),
+            new Livro( "Domain-driven design: atacando as complexidades no coração do software", "Eric Evans", 3, Acervo.AcervoPublico, EstadoLivro.Emprestado ),
+            new Livro( "A Origem das Espécies", "Charles Darwin", 1, Acervo.ForaDeEstoque, EstadoLivro.Danificado ),
+            new Livro( "Cem anos de solidão", "Gabriel García Márquez", 129, Acervo.ForaDeEstoque, EstadoLivro.Perdido )
+        };
+
     static void Main(string[] args)
     {
         Console.WriteLine("Sistema de Biblioteca do Campus");
@@ -20,6 +29,8 @@ internal class Program
             Console.Clear();
             Console.WriteLine("1 - Cadastrar usuário\n" +
                               "2 - Listar usuários\n" +
+                              "3 - Cadastrar livro\n" +
+                              "4 - Listar livros\n" +
                               "0 - Sair\n" +
                               "Digite uma opção ou 0 para sair: ");
 
@@ -43,6 +54,16 @@ internal class Program
                     MostrarUsuarios();
                     break;
 
+                case 3:
+                    Console.Clear();
+                    CadastrarLivro();
+                    break;
+
+                case 4:
+                    Console.Clear();
+                    MostrarLivros();
+                    break;
+
                 default:
                     break;
 
@@ -55,6 +76,16 @@ internal class Program
         foreach (var usuario in listaDeUsuarios)
         {
             usuario.MostrarDados();
+        }
+        Console.WriteLine("Continuar...");
+        Console.ReadLine();
+    }
+
+    static void MostrarLivros()
+    {
+        foreach (var livro in listaDeLivros)
+        {
+            livro.MostrarDados();
         }
         Console.WriteLine("Continuar...");
         Console.ReadLine();
@@ -85,5 +116,44 @@ internal class Program
         Usuario novoUsuario = new Usuario(nome, email, nivelAcesso);
 
         listaDeUsuarios.Add(novoUsuario);
+    }
+    static void CadastrarLivro()
+    {
+        string titulo, autores;
+        int edicao;
+        Acervo acervo;
+        EstadoLivro estadoLivro;
+
+        Console.WriteLine("Título do livro: ");
+        titulo = Console.ReadLine();
+
+        Console.WriteLine("\nAutor (separe os autores com vírgula): ");
+        autores = Console.ReadLine();
+        
+        Console.WriteLine("\nEdição: ");
+        while (!int.TryParse(Console.ReadLine(), out edicao))
+        {
+            Console.WriteLine("Por favor, digite um número");
+        }
+
+        Console.WriteLine("\nAcervos:");
+        foreach(Acervo tipoAcervo in Enum.GetValues(typeof(Acervo))) Console.WriteLine($"{(int)tipoAcervo} - {tipoAcervo}");
+        Console.WriteLine("Digite o código referente ao acervo: ");
+        while (!Enum.TryParse(Console.ReadLine(), out acervo))
+        {
+            Console.WriteLine("Código inválido. Digite novamente o código referente ao tipo do acervo: ");
+        }
+
+        Console.WriteLine("\nCondição do livro:");
+        foreach (EstadoLivro condicaoLivro in Enum.GetValues(typeof(EstadoLivro))) Console.WriteLine($"{(int)condicaoLivro} - {condicaoLivro}");
+        Console.WriteLine("Digite o código referente à condição atual do livro: ");
+        while (!Enum.TryParse(Console.ReadLine(), out estadoLivro))
+        {
+            Console.WriteLine("Código inválido. Digite novamente o código referente à condição atual do livro: ");
+        }
+
+        Livro novoLivro = new Livro(titulo, autores, edicao, acervo, estadoLivro);
+
+        listaDeLivros.Add(novoLivro);
     }
 }
