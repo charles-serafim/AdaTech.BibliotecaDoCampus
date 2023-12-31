4d2825c958cecd3d7ee0af564c48234e9682052a
Pedr0Siqueira <pedro_jsiqueira@hotmail.com>
2023-12-31 17:05:10 -0300

    Implementa professor

--- BibliotecaDoCampus/Usuarios/Professor.cs
+++ BibliotecaDoCampus/Usuarios/Professor.cs
@@ -11,39 +11,66 @@ namespace Usuarios
         int idProfessor;
         string codigoDeAcesso;
         string senha;
+
         public override void CancelarReserva(int idEmprestimo)
         {
-            throw new NotImplementedException();
+            this.ListarReservas().Find(x => x.idEmprestimo == idEmprestimo).estadoEmprestimo = EstadoEmprestimo.Cancelado;
         }
 
-        public override void DevolverLivro(int idEmprestimo)
+        public override void DevolverLivro(int idEmprestimo, DateTime dataDevolucao)
         {
-            throw new NotImplementedException();
+            Emprestimo emprestimo = Emprestimo.Find(x => x.idEmprestimo == idEmprestimo);
+            if (emprestimo.dataLimite > dataDevolucao)
+            {
+                emprestimo.estadoEmprestimo = EstadoEmprestimo.Finalizado;
+            }
+            else
+            {
+                emprestimo.estadoEmprestimo = EstadoEmprestimo.FinalizadoComMulta;
+                this.multaTotal += SistemaBiblioteca.CalcularMulta(emprestimo.dataLimite, dataDevolucao);
+            }
         }
 
-        public override void ExibirHistorico(List<Emprestimo> emprestimos)
+        public override List<Emprestimo> ExibirHistorico(List<Emprestimo> emprestimos)
         {
-            throw new NotImplementedException();
+            return emprestimos.FindAll(x => x.idUsuario == this.idProfessor);
         }
 
-        public override void ListarReservas(List<Emprestimo> reservas)
+        public override List<Emprestimo> ListarReservas(List<Emprestimo> reservas)
         {
-            throw new NotImplementedException();
+            return reservas.FindAll(x => x.idUsuario == this.idProfessor);
         }
 
         public override int LocalizarReserva(string? nomeLivro, int? idLivro)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(nomeLivro))
+            {
+                return ControleDeReservas.ConsultarPorNomeDoLivro(nomeLivro);
+            }
+            else if (idLivro.HasValue)
+            {
+                return ControleDeReservas.ConsultarPorIdDoLivro(idLivro.Value);
+            }
+            else
+            {
+                throw new ArgumentException("É necessário fornecer o nome do livro ou o ID do livro para realizar a busca.");
+            }
         }
 
-        public override void VerificarDisponibilidade()
+
+        public override bool VerificarDisponibilidade(Livro livro)
         {
-            throw new NotImplementedException();
+            return livro.estadoLivro == EstadoLivro.Disponivel;
         }
 
         public void SolicitarLivro(int idLivro)
         {
-            throw new NotImplementedException();
+            Livro livro = Livro.Consultar(idLivro);
+            if (livro.estadoLivro == EstadoLivro.Disponivel)
+            {
+                livro.estadoLivro = EstadoLivro.AguardandoAprovacao;
+                livro.requerente = this.idProfessor;
+            }
         }
     }
 }
