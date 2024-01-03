d9da74da5e93726f878cc1917576c9190bbd4312
Luana <luhyxi@gmail.com>
2024-01-02 22:25:13 -0300

    feats: Getters e seters adicionados a classe Livro e JsonConstructor adicionado

--- BibliotecaDoCampus/SistemaGerenciamento/Models/Livro.cs
+++ BibliotecaDoCampus/SistemaGerenciamento/Models/Livro.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SistemaGerenciamento.Models;
@@ -10,13 +11,15 @@ public class Livro
 {
     private static int s_contadorLivro = 0;
     public int IdLivro { get; set; }
-    public string _titulo;
-    public string _autores;
-    public int _edicao;
-    public Acervo _acervo;
-    public EstadoLivro _estadoLivro;
+    public string _titulo { get; set; }
+    public string[] _autores { get; set; }
+    public int _edicao { get; set; }
+    public Acervo _acervo { get; set; }
+    public EstadoLivro _estadoLivro { get; set; }
 
-    public Livro(string titulo, string autores, int edicao, Acervo acervo, EstadoLivro estadoLivro)
+
+    // Possivelmente será necessário alterrar
+    public Livro(string titulo, string[] autores, int edicao, Acervo acervo, EstadoLivro estadoLivro)
     {
         IdLivro = ++s_contadorLivro;
         _titulo = titulo;
@@ -25,6 +28,18 @@ public class Livro
         _acervo = acervo;
         _estadoLivro = estadoLivro;
     }
+
+    [JsonConstructor]
+    public Livro(int IdLivro, string _titulo, Acervo _acervo, string[] _autores, int _edicao, EstadoLivro _estadoLivro)
+    {
+        this.IdLivro = IdLivro;
+        this._titulo = _titulo;
+        this._acervo = _acervo;
+        this._autores = _autores;
+        this._edicao = _edicao;
+        this._estadoLivro = _estadoLivro;
+    }
+
 
     public void MostrarDados()
     {
