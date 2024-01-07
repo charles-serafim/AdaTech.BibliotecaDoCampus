0e4cb82ae945a1d67849cbd7b9d10bfdc18b734f
Monalisa Freitas de Brito <monalisabrito.ads@gmail.com>
2024-01-06 23:22:03 -0300

    feat: Autenticação e login de usuarios

--- BibliotecaDoCampus/SistemaGerenciamento/Autenticacao.cs
+++ BibliotecaDoCampus/SistemaGerenciamento/Autenticacao.cs
@@ -1,18 +1,86 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
-using Usuarios.Funcionarios;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaGerenciamento.Usuarios;
 
-
-namespace Usuarios
+namespace Usuarios.Funcionarios
 {
     public class Autenticacao
     {
-        public static void AutenticarUsuario(int idAtendente, string senha)
-        {
-            var funcionario = BuscarFuncionarioPorId(idAtendente);
 
-            if (funcionario == null || !ValidaSenha(senha))
+        internal byte[] GerarSenhaHash(string senha)
+        {
+            return SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(senha));
+        }
+
+        public static Usuario LogarAtendente(int idAtendente, string senha)
+        {
+            var atendente = BuscarAtendentePorId(idAtendente);
+
+            if (atendente == null)
+            {
+                throw new ArgumentException("Usuário não encontrado");
+            }
+
+            var senhaHash = GerarSenhaHash(senha);
+
+            if (atendente.senhaHash.SequenceEqual(senhaHash))
+            {
+                AtualizarSenhaExpiracao(atendente);
+                return atendente;
+            }
+
+            return null;
+
+        }
+
+        public static Usuario LogarDiretor(int idDiretor, string senha)
+        {
+            var diretor = BuscarDiretorPorId(idDiretor);
+            if (diretor == null || !ValidaSenha(senha))
+            {
+                throw new ArgumentException("Usuário não encontrado");
+            }
+
+
+            var senhaHash = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(senha));
+            if (!diretor.senhaHash.SequenceEqual(senhaHash))
+            {
+                return null;
+            }
+
+            AtualizarSenhaExpiracao(diretor);
+            return diretor;
+        }
+
+        public static Usuario LogarProfessor(int idProfessor, string senha)
+        {
+            var professor = BuscarProfessorPorId(idProfessor);
+            if (professor == null || !ValidaSenha(senha))
+            {
+                throw new ArgumentException("Usuário não encontrado");
+            }
+
+            var senhaHash = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(senha));
+
+            if (!professor.senhaHash.SequenceEqual(senhaHash))
+            {
+                return null;
+            }
+
+            AtualizarSenhaExpiracao(professor);
+            return professor;
+        }
+
+        /* aluno
+        public static Usuario LogarAluno(int idAluno, string senha)
+        {
+            var aluno = BuscarAlunoPorId(idAluno);
+            if (aluno == null || !ValidaSenha(senha))
             {
                 return "Usuário ou senha inválidos!";
             }
@@ -21,28 +89,69 @@ namespace Usuarios
                 return "Usuário autenticado com sucesso!"
             }
 
-
             byte[] hashSenhaFornecida = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(senha));
 
-            return hashSenhaFornecida.SequenceEqual(funcionario.senha);
+            return hashSenhaFornecida.SequenceEqual(aluno.senha);
 
-            funcionario.senhaExpiracao = DateTimeOffset.Now.AddDays(30);
+            aluno.senhaExpiracao = DateTimeOffset.Now.AddDays(30);
 
             return true
         }
+        */
 
-        private static Funcionario BuscarFuncionarioPorId(int idAtendente)
+        public static void AtualizarSenhaExpiracao(Usuario usuario)
         {
-            var funcionarios = File.ReadAllText("funcionarios.json");
-            var json = JsonConvert.DeserializeObjeto<List<Funcionarios>>(funcionarios);
-            var funcionario = json.FirstOrDefault(f => f.Id == idAtendente);
-
-            return funcionario;
+            usuario.senhaExpiracao = DateTimeOffset.Now.AddDays(30);
         }
+
+
+
+        private static Atendente BuscarAtendentePorId(int idAtendente)
+        {
+            var atendente = File.ReadAllText("atendente.json");
+            var json = JsonConvert.DeserializeObjeto<List<Atendente>>(atendente);
+            var atendente = json.FirstOrDefault(f => f.Id == idAtendente);
+
+            if (atendente == null)
+            {
+                throw new ArgumentException("Atendente não encontrado");
+            }
+
+            return atendente;
+        }
+
+        private static Diretor BuscarDiretorPorId(int idDiretor)
+        {
+            var diretor = File.ReadAllText("diretor.json");
+            var json = JsonConvert.DeserializeObjeto<List<Diretor>>(diretor);
+            var diretor = json.FirstOrDefault(f => f.Id == idDiretor);
+
+            return diretor;
+        }
+
+        private static Professor BuscarProfessorPorId(int idProfessor)
+        {
+            var professor = File.ReadAllText("professor.json");
+            var json = JsonConvert.DeserializeObjeto<List<Professor>>(professor);
+            var professor = json.FirstOrDefault(f => f.Id == idProfessor);
+
+            return professor;
+        }
+        /* aluno
+        private static Aluno BuscarAlunoPorId(int idAluno)
+        {
+            var aluno = File.ReadAllText("aluno.json");
+            var json = JsonConvert.DeserializeObjeto<List<Aluno>>(aluno);
+            var aluno = json.FirstOrDefault(f => f.Id == idAluno);
+
+            return aluno;
+        }
+
+        */
 
         private static bool ValidaSenha(string senha)
         {
-            if (senha.lengh < 8)
+            if (senha.Length < 8)
             {
                 return false;
             }
@@ -51,7 +160,7 @@ namespace Usuarios
             int contadorLetraMinus = 0;
             int contadorNum = 0;
 
-            for (int i = 0; i < senha.length; i++)
+            for (int i = 0; i < senha.Length; i++)
             {
                 char caractere = senha[i];
 
@@ -68,10 +177,11 @@ namespace Usuarios
                     contadorNum++;
                 }
 
-                return contadorLetraMaius >= 1 && contadorLetraMinus >= 1 && contadorNum >= 1;
+                return (contadorLetraMaius >= 1 && contadorLetraMinus >= 1 && contadorNum >= 1);
 
 
             }
         }
     }
 
+}
